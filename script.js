572b14b6e8b14c5af99c470b30908145a1cf6017
leon <alchemistflame885@gmail.com>
2024-03-07 12:23:01 -0500

    Usando all e races para resolver varias promessas

--- script.js
+++ script.js
@@ -1,18 +1,39 @@
-try{
+//com all
 
-    const myPromise = new Promise ((resolve,reject) => {
-        const nome = "nomeTeste"
-        if(nome === "nomeTeste") {
-            resolve ("O nome indicado é identico")
-        }else{
-            reject("O nome indicado não é identico")
-        }
+const p1 = new Promise((resolve, reject) => {
+    setTimeout(() => {
+        resolve("P1 OKAY")
+    }, 3000)
+})
 
-        myPromise.then((data)=>{
-            console.log(data)
-        })
-    }) 
+const p2 = new Promise((resolve, reject) => {
+    resolve("P2 OKAY")
+})
 
-}catch (err){
-    console.log(`Erro encontrado: ${err}`)
-}
+const p3 = new Promise((resolve, reject) => {
+    resolve("P3 OKAY")
+})
+
+const AllPromise = Promise.all([p1, p2, p3]).then((data) => {
+    console.log(data)
+})
+
+//com race
+
+const b1 = new Promise((resolve, reject) => {
+    setTimeout(() => {
+        resolve("B1 OKAY")
+    }, 3000)
+})
+
+const b2 = new Promise((resolve, reject) => {
+    resolve("B2 OKAY")
+})
+
+const b3 = new Promise((resolve, reject) => {
+    resolve("B3 OKAY")
+})
+
+const RacePromise = Promise.race([b1, b2, b3]).then((data) => {
+    console.log(data)
+})
